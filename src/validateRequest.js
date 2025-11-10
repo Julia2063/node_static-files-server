18916359050863f1dbec8d5d839adaffdc7f85ae
Julia2063 <sky206380@gmail.com.com>
2025-11-10 19:52:02 +0200

    My Solution

---
+++ src/validateRequest.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const validateRequest = (fileName) => {
+  const result = {
+    code: 0,
+    message: '',
+  };
+
+  if (fileName === 'file') {
+    result.code = 200;
+    result.message = "Request should start with '/file/' and contain file name";
+
+    return result;
+  }
+
+  if (!fileName.startsWith('file/')) {
+    result.code = 400;
+    result.message = "Request should start with '/file/'";
+
+    return result;
+  }
+
+  if (fileName.includes('..')) {
+    result.code = 400;
+    result.message = 'Bad Request';
+
+    return result;
+  }
+
+  if (fileName.includes('//')) {
+    result.code = 404;
+    result.message = "Request shouldn't contain duplicate slashes";
+
+    return result;
+  }
+
+  return result;
+};
+
+module.exports = {
+  validateRequest,
+};
